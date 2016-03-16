818772b53e9ebee8cb83e5412116878f6c2f7e6b
Evgeniy Vasilev <e.vasilev@office.ngs.ru>
2016-03-16 15:50:40 -0400

    project: fixings for bootstrap

--- app/scripts/bootstrap.php
+++ app/scripts/bootstrap.php
@@ -1,20 +1,22 @@
 <?php
 
+define('ROOT_DIR', dirname(__DIR__));
+define('SCRIPTS_DIR', __DIR__);
+
 /**
  * PSR-0 autoload
  */
 spl_autoload_register(function ($className) {
     $base_dir = __DIR__;
 
-    $file = __DIR__ . str_replace(['\\', '_'], '/', $className) . '.php';
+    $file = __DIR__ . '/' . str_replace(['\\', '_'], '/', $className) . '.php';
 
     if (file_exists($file)) {
         require $file;
     }
 });
 
-define('ROOT_DIR', dirname(__DIR__));
-define('COMPOSER_PATH', implode([ROOT_DIR, 'vendors' . 'autoload.php'], DIRECTORY_SEPARATOR));
+define('COMPOSER_PATH', implode([ROOT_DIR, 'vendors', 'autoload.php'], DIRECTORY_SEPARATOR));
 
 if (!file_exists(COMPOSER_PATH)) {
     throw new Exception('you need to run "composer install" command');
