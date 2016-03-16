2b1f6fb808fe48835bfe08cd1b5a2876f5fdfc76
Evgeniy Vasilev <e.vasilev@office.ngs.ru>
2016-03-16 15:50:55 -0400

    application demo

--- app/www/page.php
+++ app/www/page.php
@@ -1,2 +1,27 @@
 <?php
-phpinfo();
+
+require_once dirname(__DIR__) . '/scripts/bootstrap.php';
+
+$cache = new Memcached;
+$cache->addServer($_ENV['MEMCACHED_PORT_11211_TCP_ADDR'], $_ENV['MEMCACHED_PORT_11211_TCP_PORT']);
+if ($time = $cache->get('time')) {
+    echo 'cached time ' . $time;
+} else {
+    $time = time();
+    $cache->set('time', $time, 60);
+    echo 'setted time ' . $time;
+}
+
+$mongo = new MongoClient('mongodb://' . $_ENV['MONGO_PORT_27017_TCP_ADDR']);
+$database = $mongo->sample;
+$collection = $database->events;
+$collection->insert([
+    'type' => 'visitor',
+    'time' => new MongoDate(time())
+]);
+
+$cursor = $collection->find();
+foreach ($cursor as $document) {
+    var_dump($document);
+}
+
